42c79eb78eca14cc9f0e65157b6c4a2963a3aa70
XinZhu <chenxinzhu.xz@bytedance.com>
2022-01-26 14:01:20 +0800

    feat: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  evergreen: true,
+  title: "学习笔记",
+  description: "保持学习",
+  themeConfig: {
+    smoothScroll: true,
+    lastUpdated: "上一次更新",
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'go', link: '/go/' },
+      { text: 'External', link: 'https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E7%A6%81%E7%94%A8%E5%AF%BC%E8%88%AA%E6%A0%8F' },
+    ],
+    sidebar: {
+      '/go/': [
+        '',
+        'one'
+      ]
+    }
+  },
+}
---
+++ docs/README.md
@@ -0,0 +1,21 @@
+# Hello VuePress
+
+
+- sdfdsf
+- sfsdf
+
+#### sdfdsf
+~~~golang
+
+type Mydemo struct {
+  aa string
+}
+
+a := &Mydemo{}
+
+
+
+~~~
+
+
+- 法国的法
